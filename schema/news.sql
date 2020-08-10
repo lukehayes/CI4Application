229d79e3c431efbaaec27b59ab8fdc60ec4a984c
lukehayes <lukepurplehayes@gmail.com>
2020-08-10 19:51:45 +0100

    Added insert values to db schema

--- schema/news.sql
+++ schema/news.sql
@@ -6,3 +6,8 @@ CREATE TABLE news (
     PRIMARY KEY (id),
     KEY slug (slug)
 );
+
+INSERT INTO news VALUES
+(1,'Elvis sighted','elvis-sighted','Elvis was sighted at the Podunk internet cafe. It looked like he was writing a CodeIgniter app.'),
+(2,'Say it isn\'t so!','say-it-isnt-so','Scientists conclude that some programmers have a sense of humor.'),
+(3,'Caffeination, Yes!','caffeination-yes','World\'s largest coffee shop open onsite nested coffee shop for staff only.');
