edc9b556663bdd61d25efda2b649561d9f7cc062
lukehayes <lukepurplehayes@gmail.com>
2020-08-10 17:28:15 +0100

    PagesController now loads templates correctly

--- app/Controllers/PagesController.php
+++ app/Controllers/PagesController.php
@@ -4,6 +4,25 @@ class PagesController extends BaseController
 {
 	public function index()
 	{
-		return view('index');
+		echo view('templates/header');
+		echo view('index');
+		echo view('templates/footer');
 	}
+    
+    public function view($page = "home") 
+    {
+
+        if ( ! is_file(APPPATH.'/Views/partials/'.$page.'.php'))
+        {
+            // Whoops, we don't have a page for that!
+            throw new \CodeIgniter\Exceptions\PageNotFoundException($page);
+        }
+
+        $data['title'] = ucfirst($page); // Capitalize the first letter
+
+        echo view('templates/header', $data);
+        echo view('partials/'.$page);
+        echo view('templates/footer', $data);
+
+    }
 }
