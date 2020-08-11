a6cded685176547d807c1a54921b2f81d0c621fc
lukehayes <lukepurplehayes@gmail.com>
2020-08-11 17:42:29 +0100

    Updated posts.sql schema

--- schema/posts.sql
+++ schema/posts.sql
@@ -1,13 +1,16 @@
 DROP TABLE IF EXISTS posts;
 
 CREATE TABLE posts (
-    id int(11) NOT NULL AUTO_INCREMENT,
-    title varchar(128) NOT NULL,
-    content text NOT NULL,
-    PRIMARY KEY (id)
+    id integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+    title text NOT NULL,
+    content text NOT NULL
 );
 
-INSERT INTO posts VALUES
-(1,'Building an awesome startup with PHP','Elvis was sighted at the Podunk internet cafe. It looked like he was writing a CodeIgniter app.'),
-(2,'How I went from $0 to $10,000 monthly revenue','Scientists conclude that some programmers have a sense of humor.'),
-(3,'Caffeination, Yes!','caffeination-yes','World\'s largest coffee shop open onsite nested coffee shop for staff only.');
+INSERT INTO posts(id, title, content) VALUES
+(1,'Building an awesome startup with PHP','Elvis was sighted at the Podunk internet cafe. It looked like he was writing a CodeIgniter app.');
+
+INSERT INTO posts(id, title, content) VALUES
+(2,'How I went from $0 to $10,000 monthly revenue','Scientists conclude that some programmers have a sense of humor.');
+
+INSERT INTO posts(id, title, content) VALUES
+(3,'Caffeination, Yes!','Worlds largest coffee shop open onsite nested coffee shop for staff only.');
