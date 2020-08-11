21281fe26c390151e056740b3296dd8a610c353d
lukehayes <lukepurplehayes@gmail.com>
2020-08-11 17:42:48 +0100

    All posts now showing correctly

--- app/Views/posts/index.php
+++ app/Views/posts/index.php
@@ -5,8 +5,8 @@
     <?php foreach ($posts as $post): ?>
 
         <div class="post">
-            <h3><?php echo $post->title; ?></h3>
-            <p><?php echo $post->content; ?></p>
+            <h3><?php echo esc($post->title); ?></h3>
+            <p><?php echo esc($post->content); ?></p>
         </div>
     <?php endforeach; ?>
         
