784f5df3403f8ff879b51c385da59e6ef6730489
lukehayes <lukepurplehayes@gmail.com>
2020-08-11 11:37:07 +0100

    Updated schema/posts.sql

--- schema/posts.sql
+++ schema/posts.sql
@@ -1,13 +1,13 @@
-CREATE TABLE news (
+DROP TABLE IF EXISTS posts;
+
+CREATE TABLE posts (
     id int(11) NOT NULL AUTO_INCREMENT,
     title varchar(128) NOT NULL,
-    slug varchar(128) NOT NULL,
-    body text NOT NULL,
-    PRIMARY KEY (id),
-    KEY slug (slug)
+    content text NOT NULL,
+    PRIMARY KEY (id)
 );
 
-INSERT INTO news VALUES
-(1,'Elvis sighted','elvis-sighted','Elvis was sighted at the Podunk internet cafe. It looked like he was writing a CodeIgniter app.'),
-(2,'Say it isn\'t so!','say-it-isnt-so','Scientists conclude that some programmers have a sense of humor.'),
+INSERT INTO posts VALUES
+(1,'Building an awesome startup with PHP','Elvis was sighted at the Podunk internet cafe. It looked like he was writing a CodeIgniter app.'),
+(2,'How I went from $0 to $10,000 monthly revenue','Scientists conclude that some programmers have a sense of humor.'),
 (3,'Caffeination, Yes!','caffeination-yes','World\'s largest coffee shop open onsite nested coffee shop for staff only.');
