bea1d9d3f4d24b1b28ec8604846c4eae9e8dc9ab
lukehayes <lukepurplehayes@gmail.com>
2020-08-11 17:43:39 +0100

    Reverted routes back to PostsController

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -31,7 +31,7 @@ $routes->setAutoRoute(true);
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'PostsController::index');
-$routes->get('(:any)', 'PagesController::view/$1');
+$routes->get('/posts/(:segment)', 'PostsController::show/$1');
 
 /**
  * --------------------------------------------------------------------
