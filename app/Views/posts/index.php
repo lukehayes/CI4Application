b1de67f4e0e5e5cb3fb23882e8b374c521e8dd82
lukehayes <lukepurplehayes@gmail.com>
2020-08-11 15:11:40 +0100

    Posts are now shown in index template

--- app/Views/posts/index.php
+++ app/Views/posts/index.php
@@ -1,1 +1,14 @@
 <h3>All Posts</h3>
+
+<div class="posts">
+
+    <?php foreach ($posts as $post): ?>
+
+        <div class="post">
+            <h3><?php echo $post->title; ?></h3>
+            <p><?php echo $post->content; ?></p>
+        </div>
+    <?php endforeach; ?>
+        
+</div>
+
