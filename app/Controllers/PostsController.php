6b52235dc78cd75424a122343138b51d97866982
lukehayes <lukepurplehayes@gmail.com>
2020-08-11 17:43:06 +0100

    Added PostsController::show() method

--- app/Controllers/PostsController.php
+++ app/Controllers/PostsController.php
@@ -1,11 +1,36 @@
 <?php namespace App\Controllers;
 
+use App\Models\Post;
+
 class PostsController extends BaseController
 {
 	public function index()
 	{
+        $post = new Post();
+
 		echo view('templates/header');
-		echo view('posts/index');
+		echo view('posts/index', ['posts' => $post->findAll()]);
 		echo view('templates/footer');
 	}
+
+    public function show($id)
+    {
+        $post = new Post();
+        $result = $post->find($id);
+
+        if( is_null($result) ) {
+            throw \CodeIgniter\Exceptions\PageNotFoundException::forPageNotFound();
+        }
+
+
+
+		echo view('templates/header');
+		echo view('posts/show', ['post' => $post->find($id)]);
+		echo view('templates/footer');
+
+    }
+
+    public function view($id) {
+        var_dump($id);
+    }
 }
