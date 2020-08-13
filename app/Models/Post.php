8587dc4eed444ce8c3953b1ce3874407af3d4cb3
lukehayes <lukepurplehayes@gmail.com>
2020-08-13 11:56:36 +0100

    Moved validation rules to Post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -12,4 +12,14 @@ class Post extends Model
 
     protected $allowedFields = ['title', 'content'];
 
+    protected $validationRules    = [
+        'title'     => 'required|alpha_numeric_space|min_length[3]',
+        'content'        => 'required',
+    ];
+
+    //protected $validationMessages = [
+        //'email'        => [
+            //'is_unique' => 'Sorry. That email has already been taken. Please choose another.'
+        //]
+    //];
 }
